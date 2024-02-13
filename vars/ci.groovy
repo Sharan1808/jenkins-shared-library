bf7e3accdf935d960acfe393a1839ff714d23c88
Sharan <sharanpyata1808@gmail.com>
2024-02-13 13:22:40 -0600

    Terraform code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,11 +4,14 @@ def call() {
       sh "find . | sed -e '1d' |xargs rm -rf"
 
       if(env.TAG_NAME ==~ ".*") {
-          //env.branch_name = "refs/tags/${env.TAG_NAME}"
-          env.branch_name = "${env.TAG_NAME}"
+          env.branch_name = "refs/tags/${env.TAG_NAME}"
       } else {
+          if (env.BRANCH_NAME ==~ "PR-.*") {
+              env.branch_name = "${env.CHANGE_BRANCH}"
+          } else {
               env.branch_name = "${env.BRANCH_NAME}"
           }
+      }
 
       sh 'env'
       stage('Code Checkout') {
@@ -17,8 +20,6 @@ def call() {
                   branches: [[name: "${branch_name}"]],
                   userRemoteConfigs: [[url: "https://github.com/Sharan1808/expense-backend"]]
           )
-//          sh 'git clone https://github.com/Sharan1808/expense-backend .'
-//          sh "git checkout ${branch_name}"
           sh 'cat Jenkinsfile'
       }
 
