fe2c1faf97f9f81a1e55c3791ddaf095a475abc8
Sharan <sharanpyata1808@gmail.com>
2024-02-20 16:28:48 -0600

    Terraform code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,26 +6,8 @@ def AWS_SSM_PARAM(param_name) {
 def call() {
   node('workstation') {
 
-      sh "find . | sed -e '1d' |xargs rm -rf"
+      common.codeCheckout()
 
-      if(env.TAG_NAME ==~ ".*") {
-          env.branch_name = "refs/tags/${env.TAG_NAME}"
-      } else {
-          if (env.BRANCH_NAME ==~ "PR-.*") {
-              env.branch_name = "${env.CHANGE_BRANCH}"
-          } else {
-              env.branch_name = "${env.BRANCH_NAME}"
-          }
-      }
-
-      stage('Code Checkout') {
-//         // git branch: "${env.branchName}", url: 'https://github.com/Sharan1808/expense-backend'
-          checkout scmGit(
-                  branches: [[name: "${branch_name}"]],
-                  userRemoteConfigs: [[url: "https://github.com/Sharan1808/${repo_name}"]]
-          )
-          sh 'cat Jenkinsfile'
-      }
 
       if(app_type == "nodejs") {
           stage('Download Dependencies') {
